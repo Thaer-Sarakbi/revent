c9a0c380f71e612956f720d1df63a7c6d6466875
Thaer Sarakbi <thaersarakbi@gmail.com>
2020-05-15 22:57:57 +0200

    Updates

---
+++ precache-manifest.f16643d6c91750aa700bda633821d439.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e72f2a2d1a40b3946be763c4c493b6a7",
+    "url": "/revent/index.html"
+  },
+  {
+    "revision": "282cc5fc6d889d928734",
+    "url": "/revent/static/css/main.10fd725a.chunk.css"
+  },
+  {
+    "revision": "1ec23a98f45115297f66",
+    "url": "/revent/static/js/2.0a58104c.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/revent/static/js/2.0a58104c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "282cc5fc6d889d928734",
+    "url": "/revent/static/js/main.ec331c80.chunk.js"
+  },
+  {
+    "revision": "f891970e402dc7bbc19e",
+    "url": "/revent/static/js/runtime-main.86d894d1.js"
+  }
+]);
